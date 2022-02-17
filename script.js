54f02e6b7c4f9769b3d10e5a1d442556585862bc
Natalie Acevedo <natalieacevedo25@gmail.com>
2022-02-17 15:08:16 +0000

    added title of skill when toggled but still need to center it

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('hello mother');
+//listeners to show text when user hover over images:
+
+const allSpanImages = document.querySelectorAll(".textAbilityHide");
+const allSkillsImages = document.querySelectorAll(".icon-card");
+
+console.log(allSkillsImages);
+console.log(allSpanImages);
+
+function showSkill(e) {
+  e.preventDefault();
+
+  console.log("pepa");
+  allSpanImages.forEach((el) => {
+    el.classList.toggle("textShow");
+    e.stopPropagation();
+    console.log("si sirve");
+  });
+}
+
+allSkillsImages.forEach((el) => {
+  el.addEventListener("mouseenter", (e) => {
+    console.log(e.target);
+    showSkill(e);
+  });
+});
+
+allSkillsImages.forEach((el) => {
+  el.addEventListener("mouseleave", (e) => showSkill(e));
+});
