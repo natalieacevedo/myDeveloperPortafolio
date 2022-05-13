56b9286e8884e5384f8ba6ae597c0bd05d530e82
Natalie Acevedo <natalieacevedo25@gmail.com>
2022-05-13 11:54:37 +0100

    most functionality carrousel implemented

--- script.js
+++ script.js
@@ -27,3 +27,71 @@ navLink.forEach((link) =>
     ul.classList.remove("show");
   })
 );
+
+//carrousel funcionality
+const container = document.querySelector(".projects-container");
+const cards = document.querySelectorAll(".project-card");
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+
+
+
+rightArrow.addEventListener("click", () => {
+
+ 
+  container.scrollLeft += container.offsetWidth;
+  const activeIndicator = document.querySelector(".indicators .active");
+  if (activeIndicator.nextSibling) {
+    activeIndicator.nextSibling.classList.add("active");
+    activeIndicator.classList.remove("active");
+  }
+ 
+ 
+
+});
+
+leftArrow.addEventListener("click", () => {
+  container.scrollLeft -= container.offsetWidth;
+  const activeIndicator = document.querySelector(".indicators .active");
+  if (activeIndicator.previousSibling) {
+    activeIndicator.previousSibling.classList.add("active");
+    activeIndicator.classList.remove("active");
+  }
+});
+
+//pagination carrousel;
+const numberPages = Math.ceil(cards.length / 4);
+let indicatorsContainer = document.querySelector(".indicators");
+
+for (let i = 0; i < numberPages; i++){
+  const indicator = document.createElement("button");
+  if (i === 0) {
+    indicator.classList.add("active");
+  }
+  indicatorsContainer.appendChild(indicator);
+  indicator.addEventListener("click", (e) => {
+    container.scrollLeft = i * container.offsetWidth;
+    document.querySelector(".indicators .active").classList.remove("active");
+    e.target.classList.add("active");
+  })
+ 
+
+}
+
+//hover cards///
+
+cards.forEach(card => {
+  card.addEventListener("mouseenter", (e) => {
+    const element = e.currentTarget;
+    console.log(element);
+    setTimeout(() => {
+      cards.forEach(card => card.classList.remove("hover"));
+      element.classList.add("hover");
+    }, 300);
+   
+  })
+});
+
+container.addEventListener("mouseleave", () => {
+  cards.forEach(card => card.classList.remove("hover"));
+})
